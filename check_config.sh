5bba8e1a9f95b71b4ed818d6819963c7cb91b883
Vu Dang Ho <vudang@chotot.vn>
2020-01-26 04:25:28 +0700

    Update

--- check_config.sh
+++ check_config.sh
@@ -4,6 +4,7 @@
 #cp $HOME/{.vimrc,.zshrc,.gitconfig,.kubectl_aliases,.bash_aliases} .
 
 files=("$HOME/.config/kitty/kitty.conf" "$HOME/.config/kitty/theme.conf" "$HOME/.config/karabiner/karabiner.json" "$HOME/.config/nvim/coc-settings.json" "$HOME/.config/nvim/init.vim" "$HOME/.vimrc" "$HOME/.zshrc" "$HOME/.gitconfig" "$HOME/.kubectl_aliases" "$HOME/.bash_aliases")
+originalIFS="$IFS"
 for f in "${files[@]}"
 do
   target="${f##*/}" 
@@ -12,7 +13,7 @@ do
   omit_flag=false
   rm -f "$target"
   touch "$target"
-  while IFS= read line; do
+  while IFS= read -r line; do
     if [ "$line" == \"OMIT_START ] || [ "$line" == \#OMIT_START ]; 
     then
       omit_flag=true
@@ -28,7 +29,7 @@ do
     echo "$line" >> "$target"
   done < $f 
 done
-
+IFS="$originalIFS"
 echo "DONE"
 
 
